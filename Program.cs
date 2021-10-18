fed73eeb466e0318bffa9083b1cd818a73eb10f0
Thomas J. Lauer <tlauer2@gustavus.edu>
2021-10-18 12:58:33 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Thomas Lauer
+// MavPASS CIS 122
+// October 12, 2021
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MavPASS_CIS122_20211012
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Create a stack using queues
+			myQueue aQueue = new myQueue();
+			aQueue.Enqueue("Joker");
+			aQueue.Enqueue("Pengwing");
+			aQueue.Enqueue("Nightwin");
+
+			Console.WriteLine(aQueue.Dequeue());
+			Console.WriteLine(aQueue.Dequeue());
+			Console.WriteLine(aQueue.Dequeue());
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ myQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MavPASS_CIS122_20211012
+{
+	// Create a Queue using Stacks
+	public class myQueue
+	{
+		private Stack<string> stack1 = new Stack<string>();
+		private Stack<string> stack2 = new Stack<string>();
+
+		public void Enqueue(string aString)
+		{
+			stack1.Push(aString);
+		}
+
+		public string Dequeue()
+		{
+			// Reverse stack1 into stack2
+			int stack1Count = stack1.Count;
+			for(int i = 0; i < stack1Count; i++)
+			{
+				stack2.Push(stack1.Pop());
+			}
+
+			string returnValue = stack2.Pop();
+
+			for (int i = 0; i < stack1Count - 1; i++)
+			{
+				stack1.Push(stack2.Pop());
+			}
+
+			return returnValue;
+		}
+	}
+}
